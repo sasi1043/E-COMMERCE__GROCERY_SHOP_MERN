4f7b03aa06e1e4d3d56063b28d0db025e0ad3277
sasi1043 <sasikunar85@gmail.com>
2025-12-15 23:57:54 +0530

    final

--- grocifyreact/src/Api.js
+++ grocifyreact/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:4000"
+    baseURL:"https://e-commerce-grocery-shop-mern-1.onrender.com"
 })
 
 //to attach jwt token
